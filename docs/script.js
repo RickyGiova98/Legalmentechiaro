d04495f75281eb35e5ea67a7396b7f2c7c0c28c8
Riccardo Giovanardi <tuaemail@example.com>
2025-04-22 16:25:36 +0200

    Collegata API Glitch per traduzioni

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 
 async function traduci() {
     const input = document.getElementById("input").value;
-    const response = await fetch('/api/traduci', {
+    const response = await fetch('https://verbose-alike-can.glitch.me/traduci', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ testo: input })
